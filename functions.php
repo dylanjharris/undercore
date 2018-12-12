b0264c0441c3d02ece02cf115a128f13f0859b24
dylanjharris <admin@dylanjharris.net>
2018-12-12 09:12:43 -0800

    Adding acf support

--- functions.php
+++ functions.php
@@ -169,3 +169,11 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+
+/**
+ * Load Advanced Custom Fields file. ///// DJH Dec 12, 2018 
+ */
+if( function_exists('acf_add_options_page') ) {
+    require get_template_directory() . '/inc/acf.php';
+}
