83be2fa90239e8053fa3946426c62497e450f85b
dylanjharris <admin@dylanjharris.net>
2018-12-12 09:48:58 -0800

    Adding disable cart fragments option

--- inc/acf.php
+++ inc/acf.php
@@ -1,30 +1,68 @@
 <?php
 
 
-/*----------------------------------------------djh Nov 27, 2018
+/*----------------------------------------------djh Dec 12, 2018
   Add Theme Settings page
 ----------------------------------------------*/
-if( function_exists('acf_add_options_page') ) :
+if( function_exists('acf_add_options_page') ) {
     
-  acf_add_options_page(array(
-    'page_title'    => 'Theme Settings',
-    'menu_title'    => 'Theme Settings',
-    'menu_slug'     => 'theme-general-settings',
-    'capability'    => 'edit_posts',
-    'icon_url'      => 'dashicons-admin-settings',
-    'redirect'      => false
-  ));
+    acf_add_options_page(array(
+        'page_title'    => 'Theme Settings',
+        'menu_title'    => 'Theme Settings',
+        'menu_slug'     => 'theme-general-settings',
+        'capability'    => 'edit_posts',
+        'icon_url'      => 'dashicons-admin-settings',
+        'redirect'      => false
+    ));
   
-  // acf_add_options_sub_page(array(
-  //   'page_title'    => 'Theme Header Settings',
-  //   'menu_title'    => 'Header',
-  //   'parent_slug'   => 'theme-general-settings',
-  // ));
-  
-  // acf_add_options_sub_page(array(
-  //   'page_title'    => 'Theme Footer Settings',
-  //   'menu_title'    => 'Footer',
-  //   'parent_slug'   => 'theme-general-settings',
-  // ));
+    // acf_add_options_sub_page(array(
+        //   'page_title'    => 'Theme Header Settings',
+        //   'menu_title'    => 'Header',
+        //   'parent_slug'   => 'theme-general-settings',
+    // ));
+
+    // acf_add_options_sub_page(array(
+        //   'page_title'    => 'Theme Footer Settings',
+        //   'menu_title'    => 'Footer',
+        //   'parent_slug'   => 'theme-general-settings',
+    // ));
+}
+
+
+
+
+
+/*----------------------------------------------djh Dec 12, 2018
+  ACF OPTION: WooCommerce Cart Fragments
+----------------------------------------------*/
+if ( ! function_exists('undercore_disable_cart_fragments') ) {
+    function undercore_disable_cart_fragments() {        
+        if ( get_option( 'options_uc_disable_cart_fragments' ) ) {
+            $cart_fragments_status = get_option( 'options_uc_disable_cart_fragments' );
+            if ( $cart_fragments_status === 'disabled' ) {
+                // disable site wide
+                add_action( 'wp_print_scripts', 'undercore_disable_cart_fragments', 100 );
+            } else if ( $cart_fragments_status === 'woo_only' && is_woocommerce() ) {
+                // disable non-woocommerce only
+                add_action( 'wp_print_scripts', 'undercore_disable_cart_fragments', 100 );
+            }
+        }
+    }
     
-endif;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
