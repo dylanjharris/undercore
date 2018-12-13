9dcba3e19466889cad1e284403376d99485f163a
dylanjharris <admin@dylanjharris.net>
2018-12-12 22:18:10 -0800

    Save Point

--- functions.php
+++ functions.php
@@ -120,8 +120,9 @@ add_action( 'widgets_init', 'undercore_widgets_init' );
  * Enqueue scripts and styles.
  */
 function undercore_scripts() {
-	$version = rand(10,1000);
+	$version = undercore_current_version();
 
+	// EXAMPLES
 	// wp_enqueue_style( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, string $media = 'all' )
 	// wp_enqueue_script( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, bool $in_footer = false )
 
@@ -129,9 +130,9 @@ function undercore_scripts() {
 
 	wp_enqueue_style( 'undercore-styles', get_template_directory_uri() . '/dist/assets/css/style.css', array(), $version, 'all');
 
-	wp_enqueue_script( 'undercore-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'undercore-navigation', get_template_directory_uri() . '/js/navigation.js', array(), $version, true );
 
-	wp_enqueue_script( 'undercore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'undercore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
