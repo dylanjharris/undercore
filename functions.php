cf1ec0488984471208d6c7bcd972bcc5939fdd45
dylanjharris <admin@dylanjharris.net>
2018-12-12 22:47:25 -0800

    moved js from root to dist assets

--- functions.php
+++ functions.php
@@ -130,9 +130,9 @@ function undercore_scripts() {
 
 	wp_enqueue_style( 'undercore-styles', get_template_directory_uri() . '/dist/assets/css/style.css', array(), $version, 'all');
 
-	wp_enqueue_script( 'undercore-navigation', get_template_directory_uri() . '/js/navigation.js', array(), $version, true );
+	wp_enqueue_script( 'undercore-navigation', get_template_directory_uri() . '/dist/assets/js/navigation.js', array(), $version, true );
 
-	wp_enqueue_script( 'undercore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), $version, true );
+	wp_enqueue_script( 'undercore-skip-link-focus-fix', get_template_directory_uri() . '/dist/assets/js/skip-link-focus-fix.js', array(), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
