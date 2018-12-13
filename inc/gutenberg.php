4f99f73a95c48d21ee2327eee93243b62e92f56a
dylanjharris <admin@dylanjharris.net>
2018-12-13 12:59:23 -0800

    Adding custom editor color palette settings

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -15,33 +15,73 @@ if ( ! function_exists('undercore_gutenberg_editor_color_palette') ) {
     	// start with Foundation defaults
     	$editor_color_palette = array(
 		    array(
-		        'name' => __( 'Primary Color', 'undercore' ),
-		        'slug' => 'primary',
+		        'name'  => __( 'Primary Color', 'undercore' ),
+		        'slug'  => 'primary',
 		        'color' => '#1779ba',
 		    ),
 		    array(
-		        'name' => __( 'Secondary Color', 'undercore' ),
-		        'slug' => 'secondary',
+		        'name'  => __( 'Secondary Color', 'undercore' ),
+		        'slug'  => 'secondary',
 		        'color' => '#767676',
 		    ),
 		    array(
-		        'name' => __( 'Success Color', 'undercore' ),
-		        'slug' => 'success',
+		        'name'  => __( 'Success Color', 'undercore' ),
+		        'slug'  => 'success',
 		        'color' => '#3adb76',
 		    ),
 		    array(
-		        'name' => __( 'Warning color', 'undercore' ),
-		        'slug' => 'warning',
+		        'name'  => __( 'Warning color', 'undercore' ),
+		        'slug'  => 'warning',
 		        'color' => '#ffae00',
 		    ),
 		    array(
-		        'name' => __( 'Alert color', 'undercore' ),
-		        'slug' => 'alert',
+		        'name'  => __( 'Alert color', 'undercore' ),
+		        'slug'  => 'alert',
 		        'color' => '#cc4b37',
 		    )
 		);
-		
-
+    	// get custom color palette settings (if they exist)
+		if ( get_option( 'options_uc_editor_color_palette' ) ) { 
+			// flush the array
+			$editor_color_palette = array();
+			// get custom color settings as an array of textarea lines
+			$custom_colors = preg_split("/\r\n|\n|\r/", get_option( 'options_uc_editor_color_palette' ));
+			// recreate array with color settings
+			if ( is_array($custom_colors) ) {
+				foreach ($custom_colors as $num => $row) {
+					if ( strpos($row, ' : ') === false ) {
+						continue;
+					}
+					$color_data = explode(' : ', $row);
+					$color_hex  = '#' . substr($color_data[0], -6);
+					$color_slug = substr_replace($color_data[0],"", -7);
+					$color_label= trim($color_data[1]);
+					$editor_color_palette[] = array(
+				        'name'  => __( $color_label, 'undercore' ),
+				        'slug'  => $color_slug,
+				        'color' => $color_hex
+					);
+				}
+			}
+		}
     	return $editor_color_palette;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
